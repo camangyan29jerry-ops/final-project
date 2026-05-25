f08d6a1dcf58e2668d761869a07c3456a7506b86
camangyan29jerry-ops <camangyan29jerry@gmail.com>
2026-05-25 20:52:32 +0800

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
-// Production environment settings
-// REPLACE 'https://camangyan-auth-api.onrender.com' with your actual remote API deployment URL
+
 export const environment = {
     production: true,
     apiUrl: 'https://ipt-2026-backend-2alv.onrender.com'
